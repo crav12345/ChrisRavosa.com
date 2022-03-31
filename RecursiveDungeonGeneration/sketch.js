2865811159c8eef9378830604c8dce487bd76beb
Christopher Ravosa <Christopher.ravosa1@marist.edu>
2022-03-31 15:47:03 -0400

    Moved rendering origin out of setup

--- RecursiveDungeonGeneration/sketch.js
+++ RecursiveDungeonGeneration/sketch.js
@@ -7,29 +7,15 @@ const connectionSize = 5
 // Variables to store and determine render order of background map.
 var currLocationQueue
 let currLocation
-let previousLocation
-
-// Variables to store movement of connection lines.
-let xMod = 0
-let yMod = 0
-
-// Determines whether to draw a connection or draw a room.
-var drawRoom = true
 
 // Initialization method for p5.js library.
 function setup() {
   window.canvas = createCanvas(windowWidth, windowHeight)
   canvas.position = (0, 0)
   window.canvas.style('z-index', 1)
-  background(220)
+  background(0)
 
   currLocationQueue = generateDungeon(n, m)
-
-  currLocation = currLocationQueue.dequeue()
-
-  let c = color(0, 0, 255)
-  fill(c)
-  square((windowWidth / m) * currLocation.coordinates[0],(windowHeight / n) * currLocation.coordinates[1], roomSize)
 }
 
 // Render loop for p5.js which runs endlessly.
@@ -39,13 +25,15 @@ function draw() {
   if (!currLocationQueue.isEmpty) {
     nextLocation = currLocationQueue.dequeue()
 
-    if (nextLocation != currLocation) {
+    if (currLocation && nextLocation != currLocation) {
       c = color(255, 255, 255)
       fill(c)
+      noStroke()
       square((windowWidth / m) * currLocation.coordinates[0], (windowHeight / n) * currLocation.coordinates[1], roomSize)
 
       c = color(0, 0, 255)
       fill(c)
+      noStroke()
       square((windowWidth / m) * nextLocation.coordinates[0], (windowHeight / n) * nextLocation.coordinates[1], roomSize)
     }
 
